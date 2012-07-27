33d0a9b514772c2942ddee4938a260fcde2a6e72
Walter Dastru <walter.dastru@gmail.com>
2012-07-27 09:02:45 +0200

    start

---
+++ src/Test/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 26, 2012
+ *      Author: waleviolaeivan
+ */
+
+#include "test.h"
+
+int main(int argc, char **argv) {
+	Test test;
+	test.toUpper();
+}
+
+
+
