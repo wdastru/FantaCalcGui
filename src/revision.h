edd7ba180b6ba46468de8e7a46c9fb0356c8613c
Walter Dastru <walter.dastru@gmail.com>
2012-07-31 08:32:49 +0200

    prova 5

--- src/revision.h
+++ src/revision.h
@@ -1,6 +1,6 @@
 #ifndef REVISION_H
 #define REVISION_H
 
-THE_LOGGER->setRevision("7727b098b75573cbe82758011e8054e56a03dc68");
+THE_LOGGER->setRevision("a9ab8f148bb70582e2aaad94bdad07206176b891");
 
 #endif
