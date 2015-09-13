6ae28eb343efd0e12723e7eaef0745401c0d1d7f
Walter Dastru <walter.dastru@gmail.com>
2015-09-13 09:46:31 +0200

    mod to .gitignore

--- src/Downloader.cpp
+++ src/Downloader.cpp
@@ -137,12 +137,12 @@ void Downloader::downloadFiles() {
 		QString fullUrlString = url.scheme() + "://" + url.authority()
 				+ url.path();
 
-		//qDebug() << "In Downloader::downloadFile() --> url : " + fullUrlString;
-		//qDebug() << "In Downloader::downloadFile() --> savePaths->at(" + my::toQString<unsigned int>(i) + ") : "	+ savePaths->at(i);
+		qDebug() << "In Downloader::downloadFile() --> url : " + fullUrlString;
+		qDebug() << "In Downloader::downloadFile() --> savePaths->at(" + my::toQString<unsigned int>(i) + ") : "	+ savePaths->at(i);
 
 		if (!this->silent) {
-			//qDebug() << QObject::tr("Downloading %1").arg(fullUrlString);
-			//qDebug() << QObject::tr("Saving to %1").arg(this->savePaths->at(i));
+			qDebug() << QObject::tr("Downloading %1").arg(fullUrlString);
+			qDebug() << QObject::tr("Saving to %1").arg(this->savePaths->at(i));
 		}
 
 		this->httpClients.at(i) = new HttpWindow(this, url, this->savePaths->at(i));
@@ -150,7 +150,7 @@ void Downloader::downloadFiles() {
 
 		if (this->httpClients.at(i)->downloadSuccessful()) {
 			this->downloadSuccesses++;
-			//qDebug() << tr("In void Downloader::downloadFiles(). Download of %1 succeded.").arg(QFileInfo(this->savePaths->at(i)).fileName());
+			qDebug() << tr("In void Downloader::downloadFiles(). Download of %1 succeded.").arg(QFileInfo(this->savePaths->at(i)).fileName());
 			if (!this->silent) {
 				//LOG(INFO, tr("Scaricato %1").arg(QFileInfo(this->savePaths->at(i)).fileName()));
 			}
@@ -165,8 +165,8 @@ void Downloader::downloadFiles() {
 			LOG(ERR, tr("Scaricamento di %1 fallito.").arg(QFileInfo(this->savePaths->at(i)).fileName()));
 		}
 
-		//qDebug() << tr("Download successes : %1").arg(my::toQString<unsigned int>(this->downloadSuccesses));
-		//qDebug() << tr("Download failures : %1").arg(my::toQString<unsigned int>(this->downloadFailures));
+		qDebug() << tr("Download successes : %1").arg(my::toQString<unsigned int>(this->downloadSuccesses));
+		qDebug() << tr("Download failures : %1").arg(my::toQString<unsigned int>(this->downloadFailures));
 
 		if (this->downloadSuccesses == this->savePaths->size()) {
 			this->close();
