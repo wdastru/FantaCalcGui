7aeed1b7fe0d184cdad748c11388c14e8b3d972c
Walter Dastru <walter.dastru@gmail.com>
2015-09-13 12:51:12 +0200

    v3.3.6

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
 	QApplication::setStyle(QStyleFactory::create("cleanlooks"));
 	a.setOrganizationName("Eptadone Software");
 	a.setApplicationName("FantaCalcGui");
-	a.setApplicationVersion("3.3.4");
+	a.setApplicationVersion("3.3.6");
 
 	THE_REPO; // prima si crea il repository dei dati
 	THE_MANAGER; // quindi si legge l'ini file (o lo si crea se non esistente)
---
+++ src/revision.h
@@ -0,0 +1,6 @@
+#ifndef REVISION_H
+#define REVISION_H
+
+THE_LOGGER->setRevision("de1aedb");
+
+#endif
